2da285126abc4cf98e1ee454f990e531cf3bab8d
Pawel <jankowskipawelxd@gmail.com>
2020-06-01 18:24:59 +0200

    Make calculator working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Screen from './Screen'
 import Keyboard from './Keyboard'
 import './style.css'
+import { evaluate } from 'mathjs'
 
 class App extends Component {
 	constructor(props) {
@@ -12,13 +13,34 @@ class App extends Component {
 		}
 	}
 
+	clear = () => {
+		this.setState({ expression: "" })
+	}
+
+	deleteLast = () => {
+		this.setState({ expression: this.state.expression.slice(0, -1) })
+	}
+
+	type = char => {
+		this.setState({ expression: this.state.expression + char })
+	}
+
+	solve = () => {
+		this.setState({ expression: evaluate(this.state.expression) })
+	}
+
 	render() {
 		return (
 			<div className="Calculator">
 				<Screen 
 					expression={this.state.expression}
 				/>
-				<Keyboard />
+				<Keyboard 
+					clear={this.clear}
+					deleteLast={this.deleteLast}
+					type={this.type}
+					solve={this.solve}
+				/>
 			</div>
 		)
 	}
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -4,25 +4,25 @@ class Keyboard extends Component {
 	render() {
 		return (
 			<div className="Keyboard">
-				<button onClick={this.props.clear}>C</button>
-				<button onClick={this.props.clear}>%</button>
-				<button onClick={this.props.clear}>del</button>
-				<button onClick={this.props.clear}>/</button>
-				<button onClick={this.props.clear}>7</button>
-				<button onClick={this.props.clear}>8</button>
-				<button onClick={this.props.clear}>9</button>
-				<button onClick={this.props.clear}>x</button>
-				<button onClick={this.props.clear}>4</button>
-				<button onClick={this.props.clear}>5</button>
-				<button onClick={this.props.clear}>6</button>
-				<button onClick={this.props.clear}>-</button>
-				<button onClick={this.props.clear}>1</button>
-				<button onClick={this.props.clear}>2</button>
-				<button onClick={this.props.clear}>3</button>
-				<button onClick={this.props.clear}>+</button>
-				<button onClick={this.props.clear}>0</button>
-				<button onClick={this.props.clear}>.</button>
-				<button onClick={this.props.clear}>=</button>
+				<button onClick={() => this.props.clear()}>C</button>
+				<button onClick={() => this.props.type('%')}>%</button>
+				<button onClick={() => this.props.deleteLast()}>del</button>
+				<button onClick={() => this.props.type('/')}>/</button>
+				<button onClick={() => this.props.type('7')}>7</button>
+				<button onClick={() => this.props.type('8')}>8</button>
+				<button onClick={() => this.props.type('9')}>9</button>
+				<button onClick={() => this.props.type('*')}>x</button>
+				<button onClick={() => this.props.type('4')}>4</button>
+				<button onClick={() => this.props.type('5')}>5</button>
+				<button onClick={() => this.props.type('6')}>6</button>
+				<button onClick={() => this.props.type('-')}>-</button>
+				<button onClick={() => this.props.type('1')}>1</button>
+				<button onClick={() => this.props.type('2')}>2</button>
+				<button onClick={() => this.props.type('3')}>3</button>
+				<button onClick={() => this.props.type('+')}>+</button>
+				<button onClick={() => this.props.type('0')}>0</button>
+				<button onClick={() => this.props.type('.')}>.</button>
+				<button onClick={() => this.props.solve()}>=</button>
 			</div>
 		)
 	}
