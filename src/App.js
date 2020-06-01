35d56cefe965c63b68eb62468204afa81bc6209e
Pawel <jankowskipawelxd@gmail.com>
2020-06-01 17:45:00 +0200

    Add Screen component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import Screen from './Screen'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			expression: ""
+		}
+	}
+
 	render() {
 		return (
 			<div className="Calculator">
-				<Screen />
+				<Screen 
+					expression={this.state.expression}
+				/>
 				{/* <Keyboard /> */}
 			</div>
 		)
